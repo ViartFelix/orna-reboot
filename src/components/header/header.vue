c1a0bbe733880c95a5d98d7557ee3bab309d159c
ViartFelix <viart.felix2017@gmail.com>
2022-07-14 18:32:16 +0200

    commit setup

---
+++ src/components/header/header.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+  
+}
+</script>
+
+<template>
+  <div>
+    <h2>baguette</h2>
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//Styles
+import './assets/header/header.css'
 import './assets/main.css'
+import './assets/variables.css'
 
 const app = createApp(App)
 
