8e210bcbe7b8829afa6ccb4c550ac7d0310d1bd1
ViartFelix <viart.felix2017@gmail.com>
2022-07-14 22:19:40 +0200

    commit for save

---
+++ src/views/pets.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import { RouterLink, RouterView } from 'vue-router'
+  import header_top from '../components/header/header.vue';
+  import index_pets from '../components/pets/index.vue';
+</script>
+
+<template>
+  <header_top/>
+  <index_pets/>
+</template>
